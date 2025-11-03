5c4010f76b4296049334ed24e4d27bd3a790c162
dibyajyoti79 <dibyajyoti274@gmail.com>
2025-11-03 23:49:30 +0530

    feat: Integrate video processing for HLS streaming and add ffmpeg dependencies

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { BadRequestError } from "../utils/api-error";
 import { StatusCodes } from "http-status-codes";
 import { ApiResponse } from "../utils/api-response";
+import { processVideoForHLS } from "../services/video.service";
+import fs from "fs";
 
 export const uploadVideoController = async (req: Request, res: Response) => {
   const file = req.file;
@@ -10,8 +12,20 @@ export const uploadVideoController = async (req: Request, res: Response) => {
   }
 
   const videoPath = file.path;
-
-  res
-    .status(StatusCodes.OK)
-    .json(new ApiResponse("Video uploaded successfully", videoPath));
+  const outputPath = `output/${Date.now()}`;
+  processVideoForHLS(videoPath, outputPath, (err, masterPlaylist) => {
+    if (err) {
+      throw new BadRequestError("Error processing video", err);
+    }
+    fs.unlink(videoPath, (err) => {
+      if (err) {
+        console.error("Error deleting video:", err);
+        return;
+      }
+      console.log("Video deleted successfully");
+    });
+    res
+      .status(StatusCodes.OK)
+      .json(new ApiResponse("Video processed successfully", masterPlaylist));
+  });
 };
---
+++ src/services/video.service.ts
@@ -0,0 +1,63 @@
+import fs from "fs";
+import ffmpeg from "fluent-ffmpeg";
+
+interface Resolution {
+  width: number;
+  height: number;
+  bitrate: number;
+}
+const resolutions: Resolution[] = [
+  { width: 1920, height: 1080, bitrate: 5000000 }, // 1080p (around 5 Mbps typical)
+  { width: 1280, height: 720, bitrate: 2500000 }, // 720p (around 2.5 Mbps)
+  { width: 854, height: 480, bitrate: 1000000 }, // 480p (around 1 Mbps)
+  { width: 640, height: 360, bitrate: 750000 }, // 360p (around 0.75 Mbps)
+  { width: 426, height: 240, bitrate: 400000 }, // 240p (around 0.4 Mbps)
+  { width: 256, height: 144, bitrate: 150000 }, // 144p (around 0.15 Mbps)
+];
+
+export const processVideoForHLS = (
+  inputPath: string,
+  outputPath: string,
+  callback: (error: Error | null, masterPlaylist?: string) => void
+) => {
+  fs.mkdirSync(outputPath, { recursive: true });
+
+  const masterPlaylist = `${outputPath}/master.m3u8`;
+  const masterContent: string[] = [];
+  let countProcessing = 0;
+
+  resolutions.forEach((resolution) => {
+    const variantOutput = `${outputPath}/${resolution.height}p`;
+    const variantPlaylist = `${variantOutput}/playlist.m3u8`;
+    fs.mkdirSync(variantOutput, { recursive: true });
+    ffmpeg(inputPath)
+      .outputOption([
+        `-vf scale=w=${resolution.width}:h=${resolution.height}`,
+        `-b:v ${resolution.bitrate}k`,
+        `-codec:v libx264`,
+        `-codec:a aac`,
+        `-hls_time 10`,
+        `-hls_playlist_type vod`,
+        `-hls_segment_filename ${variantOutput}/segment%03d.ts`,
+      ])
+      .output(variantPlaylist)
+      .on("end", () => {
+        masterContent.push(
+          `#EXT-X-STREAM-INF:BANDWIDTH=${resolution.bitrate},RESOLUTION=${resolution.width}x${resolution.height}\n${resolution.height}p/playlist.m3u8"`
+        );
+        countProcessing++;
+        if (countProcessing === resolutions.length) {
+          fs.writeFileSync(
+            masterPlaylist,
+            `#EXTM3U\n${masterContent.join("\n")}`
+          );
+          callback(null, masterPlaylist);
+        }
+      })
+      .on("error", (err) => {
+        console.error("Error processing video:", err);
+        callback(err);
+      })
+      .run();
+  });
+};
